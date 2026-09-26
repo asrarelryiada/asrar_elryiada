8d181085fab198baa1c0e42eacb346319ffa7596
Mai Mahmoud <soutwerouh@gmail.com>
2026-09-26 10:43:33 +0300

    Force update web build files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "556086360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2552122239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
